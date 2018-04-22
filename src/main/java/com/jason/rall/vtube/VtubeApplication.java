bdc937a539c5d987d9498c490dfd0b10178f346e
Jason Rall <darth.json@gmail.com>
2018-04-22 15:16:11 -0600

    Initial commit

---
+++ src/main/java/com/jason/rall/vtube/VtubeApplication.java
@@ -0,0 +1,12 @@
+package com.jason.rall.vtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtubeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jason/rall/vtube/VtubeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jason.rall.vtube;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VtubeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
